a347cd161b739e690057ef3cb250079181852bad
sunilyadavfabhotel <sunil.yadav@fabhotels.com>
2024-04-24 19:20:34 +0530

    feat: basic navigation setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../../components";
+
+import "./styles.css";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+};
+
+export default HomePage;
